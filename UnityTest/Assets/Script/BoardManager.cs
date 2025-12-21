841507b459ceac4d45cf8bfaaf075b6860be6901
ACVBdeveloper <124077498+ACVBdeveloper@users.noreply.github.com>
2025-12-21 18:25:17 -0300

    bugfix BoardManager

--- UnityTest/Assets/Script/BoardManager.cs
+++ UnityTest/Assets/Script/BoardManager.cs
@@ -16,8 +16,9 @@ public class BoardManager : MonoBehaviour
             for(int j=0; j<width;j++){
                 int tileNumber = Random.Range(0, groundTiles.Length);
                 m_TileMap.SetTile(new Vector3Int(i, j, 0), groundTiles[tileNumber]);
+            }
 
-
+        }
     }
 
     // Update is called once per frame
