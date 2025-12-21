e43428e1d3dd76d3218cebb6974413357e63c5be
ACVBdeveloper <124077498+ACVBdeveloper@users.noreply.github.com>
2025-12-21 17:35:50 -0300

    building BoardManager

---
+++ UnityTest/Assets/Script/BoardManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BoardManager : MonoBehaviour
+{
+
+    private Tilemap m_TileMap;
+    public int width=8;
+    public int height=8;
+    public Tile[] groundTiles;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        m_TileMap = GetComponentInChildren<Tilemap>();
+        for( int i =0; i < height; i++){
+            for(int j=0; j<width;j++){
+                int tileNumber = Random.Range(0, groundTiles.Length);
+                m_TileMap.SetTile(new Vector3Int(i, j, 0), groundTiles[tileNumber]);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
