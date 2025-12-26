f1be8375665f7ff3288f98aeb381905afadaee22
ACVBdeveloper <124077498+ACVBdeveloper@users.noreply.github.com>
2025-12-26 18:56:40 -0300

    refactoring GenerateWall and GenerateFood method plus creating the destroy obstacle method to break scenes obstacles

--- UnityTest/Assets/Script/WallObject.cs
+++ UnityTest/Assets/Script/WallObject.cs
@@ -5,10 +5,18 @@ public class WallObject : CellObject
 {
 
     public Tile obstacleTile;
+    public int maxHealth = 3;
+
+    private int m_HealthPoint;
+    private Tile m_OriginalTile;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public override void Init(Vector2Int  cell)
     {
         base.Init(cell);
+        
+        m_HealthPoint = maxHealth;
+
+        m_OriginalTile = GameManager.Instance.boardManager.GetCellTile(cell);
         GameManager.Instance.boardManager.SetTileCell(cell,obstacleTile);
     }
 
@@ -19,6 +27,13 @@ public class WallObject : CellObject
     }
     public override bool PlayerWantsToEnter()
     {
-        return false;
+        m_HealthPoint -= 1;
+        if (m_HealthPoint>0) 
+        {
+            return false;
+        }
+        
+        GameManager.Instance.boardManager.SetTileCell(m_Cell,m_OriginalTile);
+        return true;
     }
 }
